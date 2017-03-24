379395562c56d8a17d63efe071f9ba8caa617f6b
Jan Schmutz <jan.schmutz@arcor.de>
2017-03-24 19:53:32 +0100

    automated scss compiling, css binding and minifying. Run: grunt watch

--- gruntfile.js
+++ gruntfile.js
@@ -6,19 +6,41 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
-        cssmin: {
-            my_target: {
+        watch: {
+            sass: {
+                files: ['public/components/**/*.scss'],
+                tasks: ['sass','cssmin']
+            }
+        },
+        sass: {
+            dist: {
                 files: [{
                     expand: true,
-                    cwd: 'public/css/',
-                    src: ['*.css', '!*.min.css'],
-                    dest: 'public/css',
-                    ext: '.min.css'
+                    cwd: 'public',
+                    src: ['components/**/*.scss'],
+                    dest: 'public/build/css',
+                    ext: '.css'
                 }]
+            }
+        },
+        cssmin: {
+            options: {
+                shorthandCompacting: false,
+                roundingPrecision: -1
+            },
+            combine: {
+                files: {
+                    'public/build/css/main.min.css': [
+                        'public/build/css/components/**/*.css',
+                        '!public/build/css/components/!**!/!*.min.css'
+                    ]
+                }
             }
         }
 
     });
-
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.registerTask('default', ['watch']);
 };
--- public/client.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by janschmutz on 22.03.17.
- */
