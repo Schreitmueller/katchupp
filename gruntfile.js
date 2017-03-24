af02df5849543ae5f5fb2fe2540bb5ec3ca20bbe
Jan Schmutz <jan.schmutz@arcor.de>
2017-03-24 15:49:14 +0100

    init grunt — run: npm install  — run: grunt *for default task

---
+++ gruntfile.js
@@ -0,0 +1,11 @@
+/**
+ * Created by janschmutz on 24.03.17.
+ */
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json')
+
+    })
+};
