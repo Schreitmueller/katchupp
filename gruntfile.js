0b2fd3e1b6d5932dd61c41bdbbf8c2042bc69f31
Jan Schmutz <jan.schmutz@arcor.de>
2017-03-26 19:51:16 +0200

    expand default task grunt, included nodemon shell

--- gruntfile.js
+++ gruntfile.js
@@ -42,5 +42,5 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('default', ['watch','sass','cssmin']);
 };
